91830fffa988b6b3b2da43e392d7a6b2b22bea00
changhui lee <iam@xo.dev>
2023-05-30 19:38:42 +0900

    Update neovim config

--- .config/nvim/plugins/productivity.lua
+++ .config/nvim/plugins/productivity.lua
@@ -10,4 +10,30 @@ return {
     end,
   },
   'wakatime/vim-wakatime',
+  {
+    'akinsho/toggleterm.nvim',
+    cmd = { 'ToggleTerm', 'TermExec' },
+    keys = {
+      {
+        '<C-\\>',
+        mode = { 'n', 't' },
+        silent = true,
+        function()
+          local venv = vim.b['virtual_env']
+          local term = require('toggleterm.terminal').Terminal:new({
+            env = venv and { VIRTUAL_ENV = venv } or nil,
+            count = vim.v.count > 0 and vim.v.count or 1,
+          })
+          term:toggle()
+        end,
+        desc = 'Toggle terminal',
+      },
+    },
+    opts = {
+      open_mapping = false,
+      float_opts = {
+        border = 'curved',
+      },
+    },
+  },
 }
